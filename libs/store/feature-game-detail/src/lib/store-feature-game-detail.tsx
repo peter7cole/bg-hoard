851b1a2d09ee82f9f38de50baef26ade3ec3a1de
pecole <pecole@cisco.com>
2021-08-17 14:18:57 -0700

    lab 6 done

---
+++ libs/store/feature-game-detail/src/lib/store-feature-game-detail.tsx
@@ -0,0 +1,14 @@
+import './store-feature-game-detail.module.scss';
+
+/* eslint-disable-next-line */
+export interface StoreFeatureGameDetailProps {}
+
+export function StoreFeatureGameDetail(props: StoreFeatureGameDetailProps) {
+  return (
+    <div>
+      <h1>Welcome to StoreFeatureGameDetail!</h1>
+    </div>
+  );
+}
+
+export default StoreFeatureGameDetail;
