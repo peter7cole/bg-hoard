6695b155929d6c5dd4afb6bbe2b9d568c1b5f22e
pecole <pecole@cisco.com>
2021-08-17 16:13:48 -0700

    lab 10 done

--- libs/store/ui-shared/src/lib/header/header.spec.tsx
+++ libs/store/ui-shared/src/lib/header/header.spec.tsx
@@ -2,9 +2,14 @@ import { render } from '@testing-library/react';
 
 import Header from './header';
 
-describe('Header', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Header />);
-    expect(baseElement).toBeTruthy();
-  });
+describe('store-ui-shared: Header component', () => {
+  beforeEach(() => cy.visit('/iframe.html?id=header--primary&args=title:BoardGameHoard'));
+    
+    it('should show the title', () => {
+      cy.get('header').contains('BoardGameHoard');
+    });
+    
+    it('should render the component', () => {
+      cy.get('h1').should('contain', 'Welcome to store-ui-shared!');
+    });
 });
